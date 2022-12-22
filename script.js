70227c05dd92cbc5d81bb455179c4e8eef4159d9
ajay0024 <pythontest0024@gmail.com>
2022-12-22 17:49:59 +0530

    Implement Extra Credit Color shading option

--- script.js
+++ script.js
@@ -2,11 +2,7 @@ let container = document.getElementById('block-container');
 let currentNumberOfCells = 16
 createGrid(16)
 
-// cells.forEach(element => {
-//     element.addEventListener("mouseenter",evt => {
-//         conso
-//     })
-// });
+
 function removeAllChildNodes(parent) {
     while (parent.firstChild) {
         parent.removeChild(parent.firstChild);
@@ -17,17 +13,38 @@ function createGrid(numberOfCells) {
     removeAllChildNodes(container);
     container.setAttribute('style', `grid-template-columns : repeat(${numberOfCells}, 1fr); 
     grid-template-rows : repeat(${numberOfCells}, 1fr) `)
-    // container.setAttribute('style',` `)
     for (let i = 0; i < numberOfCells * numberOfCells; i++) {
         container.appendChild(document.createElement('div'))
     }
+
+    // Each cell on mouse enter takes a random color and reduce lightness on each pass.
     [...container.children].forEach(element => {
-        element.addEventListener("mouseenter", e => {
-            console.log(e.target)
-            e.target.classList.add("green");
-            let newColor= `hsl(${Math.floor(Math.random()*255)}, `;
+        element.addEventListener("mouseenter", e => { 
+            let cell = e.target
+
+            let hue = cell.getAttribute("data-hue");
+            let sat = cell.getAttribute("data-saturation");
+            let oldLight = cell.getAttribute("data-lightness");
+            
+            let newColor;
+            if (oldLight) {
+                let newLight = oldLight - 10;
+                cell.setAttribute("data-lightness", newLight);
+                newColor = `hsl(${hue}, ${sat}%, ${newLight}%)`;
+            }
+            else{
+                let hue = Math.floor(Math.random() * 255);
+                let sat = Math.floor(Math.random() * 100);
+                cell.setAttribute("data-hue", hue);
+                cell.setAttribute("data-saturation", sat);
+                cell.setAttribute("data-lightness", 90)
+                newColor = `hsl(${hue}, ${sat}%, 90%)`;
+            }
+
             console.log(newColor);
-            e.target.setAttribute('style',`background : hsl(${Math.floor(Math.random*255)}, `)
+            
+            
+            cell.setAttribute('style', `background : ${newColor} `)
         })
     });
 
@@ -37,9 +54,11 @@ function createGrid(numberOfCells) {
 
 
 document.getElementById("cell-changer").addEventListener("click", e => {
-    let x = 16;
-    while (typeof x == "number" && x <= 100 && x > 0) {
-        x = prompt("How many cells you want on each side of square ? \n Only numbers upto 100 acceptable.");
+    
+    let  y = prompt("How many cells you want on each side of square ? \n Only numbers upto 100 acceptable.");
+    if (typeof parseInt(y) == "number" && y <= 100 & y > 0){
+        console.log(y)
+        createGrid(y)
     }
-    createGrid(x)
+    
 })
